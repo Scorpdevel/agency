dbcab105e5e0e12b5e8daf1605dc2be2a29834e0
Scorpdevel <feresjammali@googlemail.com>
2024-02-09 18:19:18 +0100

    change to maintenance

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import './App.css';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Landingsite from './pages/Landingsite';
-import Impressum from './pages/Impressum';
-import Datenschutz from './pages/Datenschutz';
+import "./App.css";
+import React, { useState } from "react";
+import Maintenance from "./pages/Maintenance";
+// import { Routes, Route } from "react-router-dom";
+// import Landingsite from "./pages/Landingsite";
+// import Impressum from "./pages/Impressum";
+// import Datenschutz from "./pages/Datenschutz";
 
 function App() {
 
+
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Landingsite />} />
-        <Route path='/impressum' element={<Impressum />} />
-        <Route path='/datenschutz' element={<Datenschutz />} />
-      </Routes>
-    </div>
+    <div style={{'maxHeight': '100vh', 'maxWidth': '100vw'}}>
+    <Maintenance />
+      {/* {maintenancemode ? (
+<div>test</div>
+      ) : (
+        <div>
+          <Routes>
+            <Route path="/" element={<Landingsite />} />
+            <Route path="/impressum" element={<Impressum />} />
+            <Route path="/datenschutz" element={<Datenschutz />} />
+          </Routes>
+        </div>
+      )} */}
+ </div>
   );
 }
 
---
+++ src/pages/Maintenance.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./maintenance.scss";
+import logo from '../assets/LogoNeu.png'
+
+const Maintenance = () => {
+  return (
+    <div className="maintenance__container">
+        <div className="container__main">
+        <div className="maintenance__left">
+      <h2>Wartungsarbeiten im Gange</h2>
+      <p>
+        Liebe Besucherinnen und Besucher, aktuell befinden sich unsere kreativen
+        Köpfe im Hintergrund in intensiven Wartungsarbeiten, um Ihr
+        Nutzungserlebnis zu verbessern. Wir bitten um Ihr Verständnis und sind
+        bald wieder für Sie da.
+      </p>
+      <p>
+      In der Zwischenzeit stehen wir Ihnen gerne über <span>info@scorpmedia.de</span> zur Verfügung. Wir danken Ihnen für Ihr Vertrauen und Ihre Geduld.
+      </p>
+      </div>
+      <div className="maintenance__right">
+        <img src={logo} width={"30%"}/>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Maintenance;
