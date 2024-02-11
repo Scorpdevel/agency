c0141785224dba3eb7cebbcf3dfd213ba43bc865
Scorpdevel <feresjammali@googlemail.com>
2024-02-11 23:34:24 +0100

    change button function

--- src/pages/Maintenance.jsx
+++ src/pages/Maintenance.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./maintenance.scss";
-import fontScorp from '../assets/Logo bright.png'
-import { Link } from "react-router-dom";
+import fontScorp from "../assets/Logo bright.png";
 
 const Maintenance = () => {
   return (
     <div className="maintenance__container">
-        <div className="container__main">
+      <div className="container__main">
         <div className="maintenance__left">
-      <h2>Coming Soon</h2>
-      <p>
-        Wartungsmodus: <br />Unsere Seite macht sich schick. <br/> 
-        Bleibt dran, wir sind bald zurück!
-      </p>
-      <Link
+          <h2>Coming Soon</h2>
+          <p>
+            Wartungsmodus: <br />
+            Unsere Seite macht sich schick. <br />
+            Bleibt dran, wir sind bald zurück!
+          </p>
+          {/* <Link
       className="buttonStyle"
             to='#'
             onClick={(e) => {
@@ -22,14 +22,19 @@ const Maintenance = () => {
             }}
         >
             Kontaktiere uns
-        </Link>
-      <div>
-        <img src={fontScorp} alt="scorpmedia schriftzug" />
-      </div>
-      <div className="info_mail">
-        <p>info@scorpmedia.de</p>
-      </div>
-      </div>
+        </Link> */}
+          <button className="buttonStyle">
+            <a href="mailto:info@scorpmedia.de?body=My custom mail body">
+              Kontaktiere uns
+            </a>
+          </button>
+          <div>
+            <img src={fontScorp} alt="scorpmedia schriftzug" />
+          </div>
+          <div className="info_mail">
+            <p>info@scorpmedia.de</p>
+          </div>
+        </div>
       </div>
     </div>
   );
